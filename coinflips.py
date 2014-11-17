94b2b3dd54a1f944e3ec6959057e5094796f6e12
Frank Huang <FrankHuang2013@outlook.com>
2014-11-17 17:02:41 -0500

    Record user's name and give them another chance to play.

--- coinflips.py
+++ coinflips.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python
 import random
 
-f = open("score.txt", "r+") # read and write
-highestScore = f.read()
-
-print("\nCoin guessing game. All time high score: {}.".format(highestScore))
-score = 0
 
 while True:
-	side = random.choice(["heads", "tails"])
-	guess = (raw_input("\nPredict heads or tails: ").lower())[0]
+
+	f = open("score.txt", "r+") # read and write
+	record = f.readlines()
+	#highestScore = f.read()
+
+	print("\nCoin guessing game. All time high score: {}, by {}".format(str(int(record[0])),record[1]))
+	score = 0
 
 	while True:
-		if guess != "h" and guess !="t":
-			guess = (raw_input("\nPlease choose from heads and tails: ").lower())[0]
-		else:
-			break
-	if guess == side[0]:
-		score += 1
-		print("You are correct! It is {}. Your present score is {}.".format(side, score))
-	else:
-		print("Sorry, it's {}. Good luck next time! Your final score is {}.".format(side, score))
-		break
+		side = random.choice(["heads", "tails"])
+		guess = (raw_input("\nPredict heads or tails: ").lower())[0]
 
-if score >= int(highestScore):
-	f.seek(0)
-	f.write(str(score))
-	print("Congratulation! You are the highest score!")
+		while True:
+			if guess != "h" and guess !="t":
+				guess = (raw_input("\nPlease choose from heads and tails: ").lower())[0]
+			else:
+				break
+		if guess == side[0]:
+			score += 1
+			print("You are correct! It is {}. Your present score is {}.".format(side, score))
+		else:
+			print("Sorry, it's {}. Good luck next time! Your final score is {}.".format(side, score))
+			break
+
+	if score >= int(record[0]):
+		f.seek(0)
+		f.write(str(score) + "\n")
+		print("/nAnd congratulation! You are the highest score!")
+		winner = raw_input("Please enter your name:")
+		record.remove(record[1])
+		f.write(winner)
+
+	f.close()
+	
+	clarify = raw_input("Do you want to play again?")
+	if (clarify.lower())[0] != "y":
+		print("Goodbye!")
+		exit()
